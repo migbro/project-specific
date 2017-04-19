432b5cccd1c57f6c97b84c3c92e31e6b8a795908
migbro <miguel.a.brown@gmail.com>
2017-04-19 10:48:12 -0500

    Added script to output coverage hole matrix from medtools hist results

---
+++ cpci/cov_hole_matrix.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import sys
+import os
+
+
+def cov_hole_matrix(hlist):
+    cov_dict = {}
+    low = 30
+    slist = []
+    for floc in open(hlist):
+        floc = floc.rstrip('\n')
+        samp = os.path.basename(floc).replace('.hist', '')
+        slist.append(samp)
+        fh = open(floc)
+        for line in fh:
+            if line[0:3] != 'all':
+                info = line.rstrip('\n').split('\t')
+                if int(info[4]) < low:
+                    if info[3] not in cov_dict:
+                        cov_dict[info[3]] = {}
+                    cov_dict[info[3]][samp] = info[4]
+            else:
+                fh.close()
+    print 'Sample/Region\t' + '\t'.join(slist)
+    for region in cov_dict:
+        sys.stdout.write(region)
+        for samp in slist:
+            if samp in cov_dict[region]:
+                sys.stdout.write('\t' + cov_dict[region][samp])
+            else:
+                sys.stdout.write('\t30')
+        print
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Create table with sample and regions on low coverage.')
+    parser.add_argument('-l', '--list', action='store', dest='hlist',
+                        help='bedtools hist table list')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    inputs = parser.parse_args()
+    hlist = inputs.hlist
+    cov_hole_matrix(hlist)
