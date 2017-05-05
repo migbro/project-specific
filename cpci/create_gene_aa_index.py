79c155427c9a06b278885dcee954b4eb12081d2c
migbro <miguel.a.brown@gmail.com>
2017-05-05 16:56:44 -0500

    Mostly debugged

--- cpci/create_gene_aa_index.py
+++ cpci/create_gene_aa_index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # created index for discordant gene/aa calls from multiple reports using a custom table generated from UCSC table browser
 import sys
+from math import ceil
 import pdb
 
 
@@ -8,23 +9,52 @@ def calc_aa_gene(ref_cur, v_pos, info):
     starts = ref_cur[5].split(',')
     ends = ref_cur[6].split(',')
     gene = ref_cur[7]
-    cur_len = 0
-    if ref_cur[4] == '+':
-        for i in xrange(len(starts)):
+    cur_aa_len = 0
+    cds_start = ref_cur[3]
+    cds_end = ref_cur[4]
+    if ref_cur[2] == '+':
+        # start could be farther or shorter in
+        f = 0
+        j = 0
+
+        while f == 0:
+            if int(starts[j]) <= int(cds_start) <= int(ends[j]):
+                f = 1
+            else:
+                j += 1
+        starts[j] = cds_start
+        for i in xrange(j, len(starts)-1, 1):
+            # starts[i] = str(int(starts[i]) + 1)
             if int(starts[i]) <= v_pos <= int(ends[i]):
-                aa_pos = str((v_pos - int(starts[i]) + cur_len)/3)
+                # pdb.set_trace()
+                aa_pos = str(int((ceil(v_pos - int(starts[i]))/3.0)) + cur_aa_len)
                 print '\t'.join(info) + '\t' + aa_pos + '\t' + gene
                 break
             else:
-                cur_len += int(ends[i]) - int(starts[i]) + 1
+                temp = (int(ends[i]) - int(starts[i]))
+                cur_aa_len += int(ceil(temp/3.0))
+
     else:
-        for i in xrange(len(starts)-2, 0, -1):
+        # start could be farther or shorter in
+        f = 0
+        j = -2
+        # pdb.set_trace()
+        while f == 0:
+            if int(starts[j]) < int(cds_end):
+                f = 1
+            else:
+                j -= 1
+        ends[j] = cds_end
+        for i in xrange(len(ends)+j, 0, -1):
+            # starts[i] = str(int(starts[i]) + 1)
+
             if int(starts[i]) <= v_pos <= int(ends[i]):
-                aa_pos = str((int(ends[i]) - v_pos + cur_len)/3)
+                aa_pos = str(int(round((int(ends[i]) - v_pos)/3.0)) + cur_aa_len)
                 print '\t'.join(info) + '\t' + aa_pos + '\t' + gene
                 break
             else:
-                cur_len += int(ends[i]) - int(starts[i]) + 1
+                temp = (int(ends[i]) - int(starts[i]))
+                cur_aa_len += int(round(temp / 3.0))
 
 to_fix = open(sys.argv[1])
 ref = open(sys.argv[2])
@@ -34,16 +64,40 @@ print 'ct\ttype\told gene\tchrom\tpos\tref\talt\taa_pos\tnew_gene'
 cur = next(ref)
 ref_cur = cur.rstrip('\n').split('\t')
 for line in to_fix:
+    sys.stderr.write('Processing ' + line)
     info = line.rstrip('\n').split('\t')
     s_flag = 0
     while s_flag == 0:
-        if info[3] == ref_cur[1]:
+        v_chr = info[3].replace('chr', '')
+        r_chr = ref_cur[1].replace('chr', '')
+        r = 0
+        v = 0
+        try:
+            v = int(v_chr)
+        except:
+            v = ord(v_chr)
+        try:
+            r = int(r_chr)
+        except:
+            r = ord(r_chr)
+
+        if r > v:
+            # pdb.set_trace()
+            sys.stderr.write('Not in predominant transcript coding region:\n' + line)
+            s_flag = 1
+        elif v_chr == r_chr:
             v_pos = int(info[4])
-            c_start = int(ref_cur[2])
-            c_end = int(ref_cur[3])
+            c_start = int(ref_cur[3])
+            c_end = int(ref_cur[4])
             if c_start <= v_pos <= c_end:
                 calc_aa_gene(ref_cur, v_pos, info)
                 s_flag = 1
+            elif c_start > v_pos:
+                sys.stderr.write('Not in predominant transcript coding region:\n' + line)
+                s_flag = 1
+            else:
+                cur = next(ref)
+                ref_cur = cur.rstrip('\n').split('\t')
         else:
             cur = next(ref)
             ref_cur = cur.rstrip('\n').split('\t')
--- cpci/get_refseq_main_tx.py
+++ cpci/get_refseq_main_tx.py
@@ -8,45 +8,39 @@ import re
 
 def write_line(fh, line):
     info = line.split('\t')
-    starts = info[3].split(',')
-    ends = info[4].split(',')
-    info.insert(2, starts[0])
-    info.insert(3, ends[-2])
     fh.write('\t'.join(info))
 
 fh = open(sys.argv[1])
 head = next(fh)
 filt = open('filtered_tx_list.txt', 'w')
 review = open('tx_to_review.txt', 'w')
-new_head = head.rstrip('\n').split('\t')
-new_head.insert(2, 'tx_start')
-new_head.insert(3, 'tx_end')
-filt.write('\t'.join(new_head) + '\n')
+filt.write(head)
 review.write(head)
 
 tx_dict = {}
 
 for line in fh:
     info = line.rstrip('\n').split('\t')
-    if info[5] not in tx_dict:
-        tx_dict[info[5]] = {}
-        tx_dict[info[5]]['flag'] = 999
-        tx_dict[info[5]]['best'] = ''
-        tx_dict[info[5]]['list'] = []
-        tx_dict[info[5]]['id_best'] = ''
-        tx_dict[info[5]]['id_flag'] = 99999999999999999999
-    tx_dict[info[5]]['list'].append(line)
-    m = re.search('transcript variant (\d+)', info[6])
+    gene = info[7]
+    if gene not in tx_dict:
+        tx_dict[gene] = {}
+        tx_dict[gene]['flag'] = 999
+        tx_dict[gene]['best'] = ''
+        tx_dict[gene]['list'] = []
+        tx_dict[gene]['id_best'] = ''
+        tx_dict[gene]['id_flag'] = 99999999999999999999
+    tx_dict[gene]['list'].append(line)
+    m = re.search('transcript variant (\d+)', info[-1])
     try:
         n = int(m.group(1))
-        if n < tx_dict[info[5]]['flag']:
-            tx_dict[info[5]]['flag'] = n
-        tx_dict[info[5]]['best'] = line
+        if n < tx_dict[gene]['flag']:
+            tx_dict[gene]['flag'] = n
+            tx_dict[gene]['best'] = line
     except:
         nm_id = info[0].split('_')
-        if int(nm_id[1]) < tx_dict[info[5]]['id_flag']:
-            tx_dict[info[5]]['id_flag'] = int(nm_id[1])
-            tx_dict[info[5]]['id_best'] = line
+        if int(nm_id[1]) < tx_dict[gene]['id_flag']:
+            tx_dict[gene]['id_flag'] = int(nm_id[1])
+            tx_dict[gene]['id_best'] = line
 
 fh.close()
 for gene in tx_dict:
