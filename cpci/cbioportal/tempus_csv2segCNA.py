168f83d4c0b80c8d544266fd49399ec0ba15f595
migbro <miguel.a.brown@gmail.com>
2018-03-15 12:33:50 -0500

    corrected output error

--- cpci/cbioportal/tempus_csv2segCNA.py
+++ cpci/cbioportal/tempus_csv2segCNA.py
@@ -12,7 +12,7 @@ for line in open(sys.argv[1]):
     info = line.rstrip('\n').split('\t')
     rename_dict[info[0]] = info[1]
 
-sys.stdout.write('ID\tchrom\tloc.start\tloc.end\tnum.mark\tseg.mean')
+sys.stdout.write('ID\tchrom\tloc.start\tloc.end\tnum.mark\tseg.mean\n')
 for fn in open(sys.argv[2]):
     fn = fn.rstrip('\n')
     cfn = os.path.basename(fn)
@@ -32,12 +32,12 @@ for fn in open(sys.argv[2]):
         end = datum[3]
         # if gene not in cnv_dict[samp] and gene != 'None':
         if amp == 'Gain':
-            print(samp + '\t' + chrom + '\t' + start + '\t' + end + '\t' + mark + cn)
+            print(samp + '\t' + chrom + '\t' + start + '\t' + end + '\t' + mark + '\t' + cn)
         else:
-            if cn != 2:
+            if cn != "2":
                 cn = int(cn)
                 if cn == 0:
                     cn += 2
                 cn = cn * -1
-                print(samp + '\t' + chrom + '\t' + start + '\t' + end + '\t' + mark + str(cn))
+                print(samp + '\t' + chrom + '\t' + start + '\t' + end + '\t' + mark + '\t' + str(cn))
     cur.close()
